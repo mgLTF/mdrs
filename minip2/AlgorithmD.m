cc9bdca0ea7050f17984db2d53ba574a9c2ac44a
Miguel Ferreira <migueltf@ua.pt>
2024-12-16 04:16:45 +0000

    up

--- minip2/AlgorithmD.m
+++ minip2/AlgorithmD.m
@@ -1,16 +1,18 @@
-function [bestSol,tbestSol,bestObjective,totalNCycles, bestNCycles,avObjective] = AlgorithmD(nNodes,Links,T,sP,nSP,timeLimit)
+function [bestSol,tbestSol,bestObjective,totalNCycles, bestNCycles,avObjective] = AlgorithmD(nNodes,Links,T,sP,nSP,timeLimit, unicastServices)
     t= tic;
     nFlows= size(T,1);
+    nFlows_unicast = nnz(ismember(T(:,1),unicastServices));
+    
     bestObjective= inf;
     totalNCycles= 0;
     aux= 0;
     while toc(t) < timeLimit
         % Greedy random algorithm
         sol= zeros(1,nFlows);
-        sol= BuildSolution(sol, nNodes,Links,T,sP,nSP,nFlows);
-        sol= HillClimbing(sol, nNodes,Links,T,sP,nSP);
+        sol= BuildSolution(sol, nNodes,Links,T,sP,nSP,nFlows, nFlows_unicast);
+        sol= HillClimbing(sol, nNodes,Links,T,sP,nSP, nFlows_unicast);
         %
-        Loads= calculateLinkLoads(nNodes,Links,T,sP,sol);
+        Loads= calculateLinkLoads(nNodes,Links,T(:,2:end),sP,sol);
         load= max(max(Loads(:,3:4)));
         totalNCycles= totalNCycles+1;
         aux= aux+load;
@@ -24,13 +26,16 @@ function [bestSol,tbestSol,bestObjective,totalNCycles, bestNCycles,avObjective] 
     avObjective= aux/totalNCycles;
 end
 
-function sol = BuildSolution(sol, nNodes,Links,T,sP,nSP,nFlows)
-    Flow_order = randperm(nFlows);
+function sol = BuildSolution(sol, nNodes,Links,T,sP,nSP,nFlows, nFlows_unicast)
+    
+    sol(nFlows_unicast+1:nFlows) = 1;
+    Flow_order = randperm(nFlows_unicast);
+    %Flow_order = randperm(nFlows);
     for f=Flow_order
         best = inf;
         for i=1:nSP(f)
             sol(f) = i;
-            Loads= calculateLinkLoads(nNodes,Links,T,sP,sol);
+            Loads= calculateLinkLoads(nNodes,Links,T(:,2:end),sP,sol);
             load= max(max(Loads(:,3:4)));
             if load < best
                 best= load;
--- minip2/task2.m
+++ minip2/task2.m
@@ -9,21 +9,27 @@ nNodes = size(Nodes,1);
 anycastNodes = [3 10];
 unicastServices = [1 2];
 
+% [3 10]
+% [1 6]
+% [4 12]
+% [5 14]
+
 v= 2e5;
 D = L/v;
 k = 6;
 [sP,nSP,totalCosts_unicast, totalCosts_anycast,T] = createPathFlows(D, T, k, unicastServices, anycastNodes);
 
 
-timeLimit = 30;
+timeLimit = 40;
 
-[bestSol,tbestSol,bestObjective,totalNCycles, bestNCycles,avObjective] = AlgorithmD(nNodes,Links,T,sP,nSP,timeLimit);
+[bestSol,tbestSol,bestObjective,totalNCycles, bestNCycles,avObjective] = AlgorithmD(nNodes,Links,T,sP,nSP,timeLimit, unicastServices);
 
-
+%%
+clc
 fprintf("Total nº Cycles = %d\n", totalNCycles);
-fprintf("Running time at which the algorithm obtains its best solution = %d s\n", tbestSol);
+fprintf("Running time at which the algorithm obtains its best solution = %.2f s\n", tbestSol);
 fprintf("The number of cycles at which the algorithm obtains its best solution. = %d\n", bestNCycles);
-
+%%
 
 nFlows_unicast = nnz(ismember(T(:,1),unicastServices));
 nServices = length(unicastServices);
@@ -38,11 +44,12 @@ for f=1:nFlows_unicast
     end
 
 end
-%%
-% Convert to milisseconds
-averageDelay = averageDelay * (10^3);
-worstDelay = worstDelay * (10^3);
 
+% Convert to milisseconds
+averageDelay = averageDelay * 2 * (10^3);
+worstDelay = worstDelay * 2 * (10^3);
+
+%%
 nFlows_s1 = 12; nFlows_s2 = 8; %TODO
 
 fprintf("Worst round-trip delay (service 1) = %.2f ms\n", worstDelay(1));
@@ -51,4 +58,7 @@ fprintf("Average round trip delay (service 1) = %.2f ms\n", averageDelay(1)/nFlo
 fprintf("Worst round-trip delay (service 2) = %.2f ms\n", worstDelay(2));
 fprintf("Average round trip delay (service 2) = %.2f ms\n", averageDelay(2)/nFlows_s1);
 
-fprintf("Worst link load of the network = %d Gbps\n", bestObjective);
+fprintf("Worst round-trip delay (service 3) = %f ms\n", max(totalCosts_anycast) * 2 * (10^3));
+fprintf("Average round trip delay (service 3) = %f ms\n", mean(totalCosts_anycast) * 2 * (10^3));
+
+fprintf("Worst link load of the network = %.2f Gbps\n", bestObjective);
